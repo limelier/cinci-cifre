81ac5ca3932f508223f8cb3fb10d8231d0a0e1bb
limelier <tudor.iacobescu.1@gmail.com>
2018-12-03 14:12:22 +0200

    Fixed really stupid bug involving badly sized array, game now actually works.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 const int PERM_LEN = 5;
 
 struct permutation {
-    unsigned short digit[4];
+    unsigned short digit[PERM_LEN];
     bool digit_used[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     bool isValid = 0;
 };
 
 struct result {
     unsigned short fixed = 0;
-    unsigned short moved = 0; 
+    unsigned short moved = 0;
 };
 
 result comparePermutations(permutation target, permutation compared) {
@@ -21,7 +21,7 @@ result comparePermutations(permutation target, permutation compared) {
 
     if (target.isValid == false || compared.isValid == false)
         throw "Received invalid permutation(s), cannot compare.";
-    
+
     for (int i = 0; i <= 9; i++) {
         if (target.digit_used[i] == true && compared.digit_used[i] == true)
             comp_result.moved++;
@@ -52,14 +52,14 @@ permutation inputPermutation() {
         }
         else {
             current_digit = (unsigned short)(input[i] - '0');
-            if (perm.digit_used[current_digit] == 1) {
+            if (perm.digit_used[current_digit] == true) {
                 perm.isValid = false;
                 throw "Input has repeated digits.";
             }
             else {
-                perm.digit_used[current_digit] = 1;
+                perm.digit_used[current_digit] = true;
                 perm.digit[i] = current_digit;
-            } 
+            }
         }
     }
     perm.isValid = true;
@@ -67,11 +67,20 @@ permutation inputPermutation() {
 }
 
 void displayPermutation(permutation perm) {
-    if (perm.isValid == false) 
+    if (perm.isValid == false)
         throw "Received invalid permutation, cannot display.";
     for (int i = 0; i < PERM_LEN; i++)
         cout << perm.digit[i];
     cout << endl;
+}
+
+void debugDisplayPermutation(permutation perm) {
+    displayPermutation(perm);
+    for (int i = 0; i <= 9; i++) {
+        cout << perm.digit_used[i];
+    }
+    cout << "; " << perm.isValid << endl;
+
 }
 
 void gameLoop() {
@@ -79,7 +88,7 @@ void gameLoop() {
 
     cout << "Please input the starting permutation. Don't let the player see it!" << endl;
     while (true) { // will keep trying to input a permutation until error is not thrown
-        try { 
+        try {
             base_perm = inputPermutation();
         } catch (const char* msg) {
             cerr << "ERROR: " << msg << endl;
@@ -98,10 +107,10 @@ void gameLoop() {
     cout << "Now, start inputting guesses. The game ends when you get it right!" << endl;
     permutation guess;
     result guess_result;
-    
+
     do {
         while (true) { // ditto
-            try { 
+            try {
                 guess = inputPermutation();
             } catch (const char* msg) {
                 cerr << "ERROR: " << msg << endl;
@@ -115,11 +124,10 @@ void gameLoop() {
             break;
         }
         guess_result = comparePermutations(base_perm, guess);
-        cout << "fixed: " << guess_result.fixed << endl;
-        cout << "moved: " << guess_result.moved << endl;
-
+        cout << "fixed: " << guess_result.fixed << "; ";
+        cout << "moved: " << guess_result.moved << ". " << endl;
     } while (guess_result.fixed != 5);
-    
+
     cout << "Congratulations, you got it!" << endl;
 }
 
