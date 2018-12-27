e24239d81ab9545e60cd364d6f651666a9cce27c
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2018-12-27 16:51:19 +0200

    Laid down the basics for a singleplayer game loop.

--- graphical_game.h
+++ graphical_game.h
@@ -1,4 +1,8 @@
 #include "base_dependencies.h"
+#include "rares_depends.h"
+
+// temporary
+#include "CLI_game.h"
 
 void guesslistPop (guesslist &list) {
     list.num--;
@@ -26,3 +30,38 @@ void guesslistPush (guesslist &list, guessnode *node) {
         list.first = node;
     }
 }
+
+guessnode *makeGuess (permutation base_perm, permutation perm) {
+    guessnode *node;
+    node = new guessnode;
+    node->perm = perm;
+    node->res = comparePermutations(base_perm, perm);
+    return node;
+}
+
+void SPGameLoop() {
+    
+    // game initialization
+    game_panel game;
+    game.base_perm = RandomPermutationGenerator();
+
+    permutation input;
+    while (game.has_been_won == false) {
+        setbkcolor(BLACK);
+        cleardevice();
+
+        drawGamePanel(300, game);
+
+        // todo: on-screen input
+        input = inputPermutation();
+
+        // todo: guesslist scrolling the other way
+        guesslistPush(game.list, makeGuess(game.base_perm, input));
+
+        // todo: being able to win
+    }
+}
+
+// todo: reverse-singleplayer, AI proof of concept
+
+// todo: 2 player 
--- main.cpp
+++ main.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include "game_graphics.h"
 #include "graphical_game.h"
-#include "rares_depends.h"
 using namespace std;
 
-void testLoop() {
-    srand(time(NULL));
-    game_panel game;
-    game.base_perm = RandomPermutationGenerator();
+// void testLoop() {
+//     srand(time(NULL));
+//     game_panel game;
+//     game.base_perm = RandomPermutationGenerator();
 
-    for (int i = 0; i < 12; i++) {
-        guessnode *node;
-        node = new guessnode;
-        node->perm = RandomPermutationGenerator();
-        node->res = comparePermutations(game.base_perm, node->perm);
-        guesslistPush(game.list, node);
-    }
+//     for (int i = 0; i < 12; i++) {
+//         guessnode *node;
+//         node = new guessnode;
+//         node->perm = RandomPermutationGenerator();
+//         node->res = comparePermutations(game.base_perm, node->perm);
+//         guesslistPush(game.list, node);
+//     }
 
-    game.active = 1;
-    drawGamePanel(20, game);
-}
+//     game.active = 1;
+//     drawGamePanel(20, game);
+// }
 
 
 int main() {
     initwindow(800,600);
 
     // drawLabel(200, 200, "fixed", false);
-    testLoop();
+    SPGameLoop();
 
     getch();
     closegraph();
