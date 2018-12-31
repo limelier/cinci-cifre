a95f9f5c5e4658739043c60bd276088b612f5002
Iacobescu Tudor <tudor.iacobescu.1@gmail.com>
2018-12-31 15:58:37 +0200

    Input panel now draws.

--- graphical_game.h
+++ graphical_game.h
@@ -2,9 +2,9 @@
 
 #include "base_dependencies.h"
 #include "rares_depends.h"
-#include "game_graphics.h"
 #include <string.h>
 #include <winbgim.h>
+#include <stack>
 
 // temporary
 #include "CLI_game.h"
@@ -27,8 +27,24 @@ const int RESULT_DIGIT_MARGIN = 25;
 const int GUESS_DIVIDER = 15;
 const int HEADER_PADDING = 5;
 const int GAMEPANEL_PADDING = 10;
-const int HEADER_TOP = 50;
+const int HEADER_TOP = 10;
 const int GAMEPANEL_DIVIDER = 10;
+
+    // input dialog
+const int INP_BOX_HEIGHT = 80;
+const int INP_BOX_BOTTOM_MARGIN = 15;
+const int INP_BOX_BOTTOM = WINDOW_HEIGHT - INP_BOX_BOTTOM_MARGIN;
+const int INP_BOX_TOP = INP_BOX_BOTTOM - INP_BOX_HEIGHT;
+const int INP_BOX_HORIZ_PADDING = 50;
+const int INP_BOX_VERT_PADDING = 10;
+const int INP_BOX_SPACING = 10;
+const int CE_BTN_WIDTH = 50;
+const int C_BTN_WIDTH = 50;
+const int ENTER_BTN_WIDTH = 100;
+const int INP_BUTTON_FONTSIZE = 3;
+const int INP_NUMPANEL_FONTSIZE = 5;
+
+
 
 const char LABEL1[] = "fixed";
 const char LABEL2[] = "moved";
@@ -61,6 +77,14 @@ const RGB_color _GREY {100, 100, 100};
 #define GREYED_BACKGROUND _BLUEGRAY
 #define NORMAL_BACKGROUND _BLUE
 
+#define INP_BACKING _DK_BLUEGRAY
+#define INP_NUMPANEL _BLACK
+#define INP_NUMPANEL_TEXT _WHITE
+#define INP_BUTTON_BG _BLUEGRAY
+#define INP_BUTTON_FG _BLACK
+#define INP_BUTTON_BG_HOVER _BLUE
+#define INP_BUTTON_FG_HOVER _BLACK
+
 void setcolorRGB(RGB_color color) {
     setcolor(RGB(color.r, color.g, color.b));
 }
@@ -73,7 +97,7 @@ void setfillstyleFlatRGB(RGB_color color) {
     setfillstyle(1, RGB(color.r, color.g, color.b));
 }
 
-// BASE FUNCTIONS
+// BASIC DRAW FUNCTIONS
 
 void drawFilledRect(int left, int top, int right, int bottom) {
     int poly_points[8] = 
@@ -86,7 +110,6 @@ void drawFilledRect(int left, int top, int right, int bottom) {
 
     fillpoly(4, poly_points);
 }
-
 
 // currently behaves oddly in corners. usage postponed.
 void drawFilledRoundedRect(int left, int top, int right, int bottom, int radius) {
@@ -136,7 +159,6 @@ struct button {
 
     button_graphics graph;
 };
-
 
 void drawButton(button btn) {
     RGB_color bg_color = btn.hover ? btn.graph.bg_hover : btn.graph.bg;
@@ -375,15 +397,98 @@ void buttonLoopStep(button &btn) {
     }
 }
 
+void drawInputBacking() {
+    setcolorRGB(INP_BACKING);
+    setfillstyleFlatRGB(INP_BACKING);
+    drawFilledRect(1, INP_BOX_TOP, WINDOW_WIDTH, INP_BOX_BOTTOM);
+}
+
+void drawInputNumpanel() {
+    setcolorRGB(INP_NUMPANEL);
+    setfillstyleFlatRGB(INP_NUMPANEL);
+    int bottom = INP_BOX_BOTTOM - INP_BOX_VERT_PADDING;
+    int top = INP_BOX_TOP + INP_BOX_VERT_PADDING;
+    int left = INP_BOX_HORIZ_PADDING;
+    int right = WINDOW_WIDTH - INP_BOX_HORIZ_PADDING;
+    right -= ENTER_BTN_WIDTH + C_BTN_WIDTH + CE_BTN_WIDTH;
+    right -= 3 * INP_BOX_SPACING;
+    drawFilledRect(left, top, right, bottom);
+}
+
+void drawInputStack(stack <int> input_stack) {
+    stack <int> reverse_input_stack;
+    char text[PERM_LEN + 1] = "     ";
+
+    while (input_stack.empty() == false) {
+        text[input_stack.size() - 1] = input_stack.top() + '0';
+        input_stack.pop();
+    }
+
+    int top = INP_BOX_TOP + INP_BOX_VERT_PADDING;
+    int bottom = INP_BOX_BOTTOM - INP_BOX_VERT_PADDING;
+    int left = INP_BOX_HORIZ_PADDING;
+
+    setcolorRGB(INP_NUMPANEL_TEXT);
+    setbkcolorRGB(INP_NUMPANEL);
+    settextstyle(COMPLEX_FONT, HORIZ_DIR, INP_NUMPANEL_FONTSIZE);
+    top = (top + bottom - textheight(text)) / 2;
+    outtextxy(left + 20, top + 2, text);
+}
 
 permutation inputPermutation2() {
     permutation input;
     bool perm_complete = false;
+    stack <int> input_stack;
 
-    // drawInputDialog();
+    int inp_box_bottom = WINDOW_HEIGHT - INP_BOX_BOTTOM_MARGIN;
+    int inp_box_top = inp_box_bottom - INP_BOX_HEIGHT;
+
+    // button initialization. i hate everything about this part.
+    button btn_enter;
+    btn_enter.right = WINDOW_WIDTH - INP_BOX_HORIZ_PADDING;
+    btn_enter.left = btn_enter.right - ENTER_BTN_WIDTH;
+    btn_enter.top = inp_box_top + INP_BOX_VERT_PADDING;
+    btn_enter.bottom = inp_box_bottom - INP_BOX_VERT_PADDING;
+    strcpy(btn_enter.graph.text, "Enter");
+    btn_enter.graph.bg = INP_BUTTON_BG;
+    btn_enter.graph.fg = INP_BUTTON_FG;
+    btn_enter.graph.bg_hover = INP_BUTTON_BG_HOVER;
+    btn_enter.graph.fg_hover = INP_BUTTON_FG_HOVER;
+
+    button btn_CE;
+    btn_CE.right = btn_enter.left - INP_BOX_SPACING;
+    btn_CE.left = btn_CE.right - CE_BTN_WIDTH;
+    btn_CE.top = inp_box_top + INP_BOX_VERT_PADDING;
+    btn_CE.bottom = inp_box_bottom - INP_BOX_VERT_PADDING;
+    strcpy(btn_CE.graph.text, "CE");
+    btn_CE.graph.bg = INP_BUTTON_BG;
+    btn_CE.graph.fg = INP_BUTTON_FG;
+    btn_CE.graph.bg_hover = INP_BUTTON_BG_HOVER;
+    btn_CE.graph.fg_hover = INP_BUTTON_FG_HOVER;
+
+    button btn_C;
+    btn_C.right = btn_CE.left - INP_BOX_SPACING;
+    btn_C.left = btn_C.right - C_BTN_WIDTH;
+    btn_C.top = inp_box_top + INP_BOX_VERT_PADDING;
+    btn_C.bottom = inp_box_bottom - INP_BOX_VERT_PADDING;
+    strcpy(btn_C.graph.text, "C");
+    btn_C.graph.bg = INP_BUTTON_BG;
+    btn_C.graph.fg = INP_BUTTON_FG;
+    btn_C.graph.bg_hover = INP_BUTTON_BG_HOVER;
+    btn_C.graph.fg_hover = INP_BUTTON_FG_HOVER;
+
+    // draw input dialog
+    drawInputBacking();
+    drawInputNumpanel();
+    drawInputStack(input_stack);
+    drawButton(btn_enter);
+    drawButton(btn_C);
+    drawButton(btn_CE);
 
     while (perm_complete == false) {
-        // drawInputDialog();
+        buttonLoopStep(btn_enter);
+        buttonLoopStep(btn_CE);
+        buttonLoopStep(btn_C);
         // < look for key hits 0-9
         // << if pressed, and not complete, add character
         // < look for button hits: enter, erase and clear
--- main.cpp
+++ main.cpp
@@ -3,39 +3,10 @@
 #include "graphical_game.h"
 using namespace std;
 
-// void testLoop() {
-//     srand(time(NULL));
-//     game_panel game;
-//     game.base_perm = RandomPermutationGenerator();
-
-//     for (int i = 0; i < 12; i++) {
-//         guessnode *node;
-//         node = new guessnode;
-//         node->perm = RandomPermutationGenerator();
-//         node->res = comparePermutations(game.base_perm, node->perm);
-//         guesslistPush(game.list, node);
-//     }
-
-//     game.active = 1;
-//     drawGamePanel(20, game);
-// }
-
-
 int main() {
     initwindow(800,600);
 
-    SPGameLoop();
-
-    button btn;
-    btn.left = 100;
-    btn.right = 300;
-    btn.top = 100;
-    btn.bottom = 200;
-
-    drawButton(btn);
-    while(true) {
-        buttonLoopStep(btn);
-    }
+    inputPermutation2();
 
     getch();
     closegraph();
